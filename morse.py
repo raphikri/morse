9b85e7cd3c8ceb23f3abcdfe337bf25f4b4df1c5
Raphael KRIEF <kriefraphael88@gmail.com>
2020-10-06 16:26:02 +0200

    100% passing tests for one-word Morse decoder

--- morse.py
+++ morse.py
@@ -1,5 +1,23 @@
 # morse.py
 # pylint: disable=missing-docstring
 
+CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.',
+        }
+
+CODE_REVERSED = {value:key for key,value in CODE.items()}
+
 def decode(message):
-    pass # TODO: implement the behavior!
+    return ''.join(CODE_REVERSED.get(i) for i in message.split())
